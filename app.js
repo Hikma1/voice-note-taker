8d4d2baa86d842f8e634c860b22de6efd04cf841
Hikma1 <hikma0227@gmail.com>
2025-11-17 21:51:08 +0300

    js added

---
+++ app.js
@@ -0,0 +1,5 @@
+// Grab elements from HTML (DOM)
+const startBtn = document.querySelector('#startBtn');
+const saveBtn = document.querySelector('#saveBtn');
+const transcript = document.querySelector('#transcript');
+const notesList = document.querySelector('#notesList');
